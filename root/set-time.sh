0e37676f198f68f2fa11106b07edd05847576765
Charles E Vopicka <51468794+cvopicka@users.noreply.github.com>
2023-08-31 21:03:05 -0600

    Fix IF statement failure

--- root/set-time.sh
+++ root/set-time.sh
@@ -6,21 +6,21 @@ while ! $synced; do
 
     # Verify that the time is correctly set to the new time zone
 
-	if ping -c 1 -W 3 pool.ntp.org &> /dev/null; then
-		echo "The network connection is normal"
+    if ping -c 1 -W 3 pool.ntp.org > /dev/null 2>&1; then
+        echo "The network connection is normal"
 
-		# Perform NTP time synchronization
-		current_time=$(date)
-		echo "Pre-sync time: $current_time"
-		/usr/sbin/ntpdate pool.ntp.org >/dev/null 2>&1
-		synced=true
-		current_time=$(date)
-    	echo "Current time: $current_time"
-		echo "Time synced successfully"
-	else
-		echo "unable to connect to network"
-		# set delay
-    	sleep 30
-	fi
+        # Perform NTP time synchronization
+        current_time=$(date)
+        echo "Pre-sync time: $current_time"
+        /usr/sbin/ntpdate pool.ntp.org >/dev/null 2>&1
+        synced=true
+        current_time=$(date)
+    echo "Current time: $current_time"
+        echo "Time synced successfully"
+    else
+        echo "unable to connect to network"
+        # set delay
+        sleep 30
+    fi
 
 done
