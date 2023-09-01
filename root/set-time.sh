57ef79b3d7d1cfe3a22394953188533b1dd17d3e
Charles E Vopicka <51468794+cvopicka@users.noreply.github.com>
2023-08-31 20:51:58 -0600

    Clean up

--- root/set-time.sh
+++ root/set-time.sh
@@ -2,29 +2,25 @@
 
 synced=false
 
-while true; do
+while ! $synced; do
 
     # Verify that the time is correctly set to the new time zone
-    current_time=$(date)
-    echo "Current time: $current_time"
 
-    # set delay
-    sleep 3600
+	if ping -c 1 -W 3 pool.ntp.org &> /dev/null; then
+		echo "The network connection is normal"
 
-	if ! $synced; then
-
-		if ping -c 1 -W 3 pool.ntp.org &> /dev/null; then
-			echo "The network connection is normal"
-
-			# Perform NTP time synchronization
-			current_time=$(date)
-			echo "Pre-sync time: $current_time"
-			/usr/sbin/ntpdate pool.ntp.org >/dev/null 2>&1
-			synced=true
-			echo "Time synced successfully"
-		else
-			echo "unable to connect to network"
-		fi
+		# Perform NTP time synchronization
+		current_time=$(date)
+		echo "Pre-sync time: $current_time"
+		/usr/sbin/ntpdate pool.ntp.org >/dev/null 2>&1
+		synced=true
+		current_time=$(date)
+    	echo "Current time: $current_time"
+		echo "Time synced successfully"
+	else
+		echo "unable to connect to network"
+		# set delay
+    	sleep 30
 	fi
 
 done
